796a49edb270ad3c5591ff117f8f26ba7a363a1d
mMckibbin1 <54712429+mMckibbin1@users.noreply.github.com>
2019-09-09 15:58:04 +0100

    Adding initial file

---
+++ Test/main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import test
+import GUI
+
+#root = tk.Tk()
+
+#app = GUI.bookwedding(root)
+#root.mainloop()
+
+GUI.mainMenu()
+
+
+weddingList = []
+
+for i in weddingList:
+    print(i.contactNo)
