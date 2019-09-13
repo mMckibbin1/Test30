0d6f70abeef177f6c8ce9ee2a5180a2d47efc91c
hking21 <54800997+hking21@users.noreply.github.com>
2019-09-13 09:24:17 +0100

    fixed main menu form

--- Test/GUI.py
+++ Test/GUI.py
@@ -42,7 +42,7 @@ class mainMenu:
         main_menu.config(background="powder blue")
 
         #adding UI elements to the form
-        Label(mainMenu, text="Please select what you would like to do...",font=("arial",15,"bold"), bg="powder blue")\
+        Label(main_menu, text="Please select what you would like to do...",font=("arial",15,"bold"), bg="powder blue")\
             .grid(row=0, pady=(25, 0), padx=(10,10))
 
         btnbookWedding = Button(main_menu, text="Add wedding Booking", width=23, bg="medium aquamarine",
