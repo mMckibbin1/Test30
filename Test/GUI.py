eb190fc02a30420e54c777a7a337385e98834599
KG-26 <54800880+KG-26@users.noreply.github.com>
2019-09-13 15:27:57 +0100

    Updated GUI colour scheme

--- Test/GUI.py
+++ Test/GUI.py
@@ -48,28 +48,29 @@ class mainMenu:
         # creates form, removes maximise button on form, adds form title and adds background colour
         main_menu = Tk()
         main_menu.resizable(0, 0)
-        main_menu.title("Main Menu")
-        main_menu.config(background="powder blue")
+        main_menu.title("Hotel Booking System - Main Menu")
+        main_menu.config(background="#70ABAF")
 
         #adding UI elements to the form
 
         #Main menu Title
 
-
-        Label(main_menu, text="Please select what you would like to do...",font=("arial",15,"bold"), bg="powder blue")\
-            .grid(row=0, pady=(25, 0), padx=(10,10))
+        Label(main_menu, text="Welcome!", font=("arial", 30, "bold"), bg="#70ABAF") \
+            .grid(row=0, pady=(25, 0), padx=(10, 10))
+        Label(main_menu, text="Please select what you would like to do today",font=("arial",14), bg="#70ABAF")\
+            .grid(row=1, pady=(25, 0), padx=(10,10))
 
         #Main menu buttons with styling that redirect to our other windows
-        btnbookWedding = Button(main_menu, text="Add wedding Booking", width=23, bg="medium aquamarine",command=call_wedding_popup)
-        btnbookParty = Button(main_menu, text="Add Party Booking", width=23, bg="medium aquamarine", command=call_party_popup)
-        btnbookConference = Button(main_menu, text="Add Conference Booking", width=23, bg="medium aquamarine",command=call_conference_popup)
-        btnviewbookings = Button(main_menu, text="View, edit and delete Bookings", width=23, bg="medium aquamarine",command=viewbooking.call_viewBookings_popup)
+        btnbookWedding = Button(main_menu, text="Add wedding Booking", width=30,height=4, bg="medium aquamarine",command=call_wedding_popup)
+        btnbookParty = Button(main_menu, text="Add Party Booking", width=30,height=4, bg="medium aquamarine", command=call_party_popup)
+        btnbookConference = Button(main_menu, text="Add Conference Booking", width=30,height=4, bg="medium aquamarine",command=call_conference_popup)
+        btnviewbookings = Button(main_menu, text="View, edit and delete Bookings", width=30,height=4, bg="medium aquamarine",command=viewbooking.call_viewBookings_popup)
 
         #Main menu buttons being placed using grid layout
-        btnbookWedding.grid(row=1, column=0, pady=(25, 5))
-        btnbookParty.grid(row=2, column=0, pady=(25, 5))
-        btnbookConference.grid(row=3, column=0, pady=(25, 5))
-        btnviewbookings.grid(row=4, column=0, pady=(25, 25))
+        btnbookWedding.grid(row=2, column=0, pady=(25, 5))
+        btnbookParty.grid(row=3, column=0, pady=(25, 5))
+        btnbookConference.grid(row=4, column=0, pady=(25, 5))
+        btnviewbookings.grid(row=5, column=0, pady=(25, 25))
 
         #calling the main menu loop
         main_menu.mainloop()
@@ -92,29 +93,30 @@ class bookwedding:
         #Creation of wedding form set title, size ect..
         master.title("Wedding bookings")
         master.resizable(0, 0)
-        master.config(background="powder blue")
+        master.config(background="#70ABAF")
 
         #defines options for dropdown boxes
         RoomOption = ['H', 'I']
         BandNames = ["Lil' Febrezey", "Prawn Mendes", "AB/CD"]
 
         DefaultRoomNo = StringVar(master)
-        DefaultRoomNo.set(RoomOption[0])  # default value
+        DefaultRoomNo.set("Please Select A Room ")  # default value
         DefaultBandName = StringVar(master)
-        DefaultBandName.set(BandNames[0])  # default value
+        DefaultBandName.set("Please Select A Band")  # default value
 
 
         #Labels for Wedding booking form
-        Label(master, text="Please fill in the details for the wedding event you are booking",font=("arial", 15, "bold"), bg="powder blue").grid(row=0, pady=(25, 0), padx=(10, 10), columnspan=4)
+        Label(master, text="Book a New Wedding", font=("arial", 20, "bold"), bg="#70ABAF", anchor='center').grid(row=0, columnspan=2, pady=(25, 0), padx=(10, 10))
+        Label(master, text="Please fill in all the details below!",font=("arial", 14), fg="blue", bg="#70ABAF").grid(row=1, pady=(25, 0), padx=(30, 10), columnspan=4, sticky=W)
 
-        Label(master, text="Number of guest", font=("arial", 10, "bold"), bg="powder blue").grid(row=1, columnspan=2, pady=(25, 0), padx=(10, 10))
-        Label(master, text="Name of contact", font=("arial", 10, "bold"), bg="powder blue").grid(row=2,columnspan=2,pady=( 25, 0), padx=(10, 10))
-        Label(master, text="Address", font=("arial", 10, "bold"), bg="powder blue").grid(row=3, columnspan=2,pady=(25, 0),padx=(10, 10))
-        Label(master, text="Contact number", font=("arial", 10, "bold"), bg="powder blue").grid(row=4, columnspan=2,pady=(25, 0),padx=(10, 10))
-        Label(master, text="Event Room Number", font=("arial", 10, "bold"), bg="powder blue").grid(row=5,columnspan=2,pady=(25, 0),padx=(10,10))
-        Label(master, text="Date of event", font=("arial", 10, "bold"), bg="powder blue").grid(row=6,columnspan=2,pady=(25, 0),padx=(10, 10))
-        Label(master, text="Band Name", font=("arial", 10, "bold"), bg="powder blue").grid(row=8, columnspan=2, pady=(25, 0), padx=(10, 10))
-        Label(master, text="Number of bedrooms reserved", font=("arial", 10, "bold"), bg="powder blue").grid(row=9, columnspan=2, pady=(25, 0), padx=(10, 10))
+        Label(master, text="Number of guest", font=("arial", 10, "bold"), bg="#70ABAF").grid(row=2, columnspan=2, pady=(25, 0), padx=(10, 10))
+        Label(master, text="Name of contact", font=("arial", 10, "bold"), bg="#70ABAF").grid(row=3,columnspan=2,pady=( 25, 0), padx=(10, 10))
+        Label(master, text="Address", font=("arial", 10, "bold"), bg="#70ABAF").grid(row=4, columnspan=2,pady=(25, 0),padx=(10, 10))
+        Label(master, text="Contact number", font=("arial", 10, "bold"), bg="#70ABAF").grid(row=5, columnspan=2,pady=(25, 0),padx=(10, 10))
+        Label(master, text="Event Room Number", font=("arial", 10, "bold"), bg="#70ABAF").grid(row=6,columnspan=2,pady=(25, 0),padx=(10,10))
+        Label(master, text="Date of event", font=("arial", 10, "bold"), bg="#70ABAF").grid(row=7,columnspan=2,pady=(25, 0),padx=(10, 10))
+        Label(master, text="Band Name", font=("arial", 10, "bold"), bg="#70ABAF").grid(row=8, columnspan=2, pady=(25, 0), padx=(10, 10))
+        Label(master, text="Number of bedrooms reserved", font=("arial", 10, "bold"), bg="#70ABAF").grid(row=9, columnspan=2, pady=(25, 0), padx=(10, 10))
 
         #Entry boxes, dropdowns and datepicker for wedding form
         self.EntnumberOfguest = Entry(master, font=("arial", 10), width=50)
@@ -129,18 +131,18 @@ class bookwedding:
         self.EntBedroomReserved = Entry(master, font=("arial", 10), width=50)
 
         # Entry boxes, dropdowns and datepicker for wedding form being placed using grid layout
-        self.EntnumberOfguest.grid(row=1, column=2, columnspan=2, sticky=W, pady=(25, 0), padx=(0, 25))
-        self.EntnameOfContact.grid(row=2, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
-        self.EntAddress.grid(row=3, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
-        self.EntContactNumber.grid(row=4, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
-        self.OpmEventRoomNumber.grid(row=5, column=2, columnspan=2, pady=(25, 0), padx=(0, 25), sticky="ew")
-        self.CalDateOfEvent.grid(row=6, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
+        self.EntnumberOfguest.grid(row=2, column=2, columnspan=2, sticky=W, pady=(15, 0), padx=(0, 25))
+        self.EntnameOfContact.grid(row=3, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
+        self.EntAddress.grid(row=4, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
+        self.EntContactNumber.grid(row=5, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
+        self.OpmEventRoomNumber.grid(row=6, column=2, columnspan=2, pady=(25, 0), padx=(0, 25), sticky="ew")
+        self.CalDateOfEvent.grid(row=7, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
         self.OpmBandName.grid(row=8, column=2, columnspan=2, pady=(25, 0), padx=(0, 25), sticky="ew")
         self.EntBedroomReserved.grid(row=9, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
 
         #Buttons for Add and Cancel on the wedding form
-        btnCloseForm = Button(master, text="Cancel", command=master.destroy)
-        btnAddBooking = Button(master, text="Add Booking",
+        btnCloseForm = Button(master, text="Cancel",bg="medium aquamarine", width=30,height=3, command=master.destroy)
+        btnAddBooking = Button(master, text="Add Booking",bg="medium aquamarine", width=30,height=3,
                                command=lambda: [test.createwedding(self.EntnumberOfguest.get(),
                                                                    self.EntnameOfContact.get(),
                                                                    self.EntAddress.get(),
@@ -150,8 +152,8 @@ class bookwedding:
                                                                    self.bandName,
                                                                    self.EntBedroomReserved.get()), master.destroy()])
         ##Buttons for Add and Cancel on the wedding form being placed using grid layout
-        btnAddBooking.grid(row=10, column=1, columnspan=1, pady=(25, 50), padx=(0, 25), sticky="ew")
-        btnCloseForm.grid(row=10, column=3, columnspan=2, pady=(25, 50), padx=(0, 50), sticky="ew")
+        btnAddBooking.grid(row=10, column=1, columnspan=1, pady=(50, 50), padx=(75, 25), sticky="ew")
+        btnCloseForm.grid(row=10, column=3, columnspan=2, pady=(50, 50), padx=(25, 75), sticky="ew")
 
     #function to get room number from dropdown
     def getRoomnumber(self, value):
@@ -202,9 +204,9 @@ class bookParty:
         BandNames = ["Lil' Febrezey", "Prawn Mendes", "AB/CD"]
 
         DefaultRoomNo = StringVar(master)
-        DefaultRoomNo.set(RoomOption[0])  # default value
+        DefaultRoomNo.set("Please Select A Room")  # default value
         DefaultBandName = StringVar(master)
-        DefaultBandName.set(BandNames[0])  # default value
+        DefaultBandName.set("Please Select A Band")  # default value
 
         # Labels for Party booking form
         Label(master, text="Please fill in the details for the Party event you are booking",font=("arial", 15, "bold"), bg="powder blue").grid(row=0, pady=(25, 0), padx=(10, 10), columnspan=4)
@@ -313,7 +315,7 @@ class bookConference:
         RoomOption = ['A', 'B', 'C']
 
         DefaultRoomNo = StringVar(master)
-        DefaultRoomNo.set(RoomOption[0])  # default value
+        DefaultRoomNo.set("Please Select a Room")  # default value
 
         # Labels for Conference booking form
         Label(master, text="Please fill in the details for the Conference event you are booking",font=("arial", 15, "bold"), bg="powder blue").grid(row=0, pady=(25, 0), padx=(10, 10), columnspan=4)
@@ -343,7 +345,7 @@ class bookConference:
 
         #checkbox now works :)
         CheckVar1 = IntVar()
-        self.chxProjectorRequired = Checkbutton(master, text='', variable=CheckVar1, onvalue=True, offvalue=False, command=ch_box_sel)
+        self.chxProjectorRequired = Checkbutton(master, text='', variable=CheckVar1, onvalue=True, offvalue=False, bg="powder blue", command=ch_box_sel)
 
         # Entry boxes, dropdowns and datepicker for conference form being placed using a grid layout
         self.EntnumberOfguest.grid(row=1, column=2, columnspan=2, pady=(25, 0), padx=(0, 25))
